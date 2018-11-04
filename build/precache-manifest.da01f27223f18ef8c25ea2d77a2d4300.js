589d68d4d6b6b598302aa970d9260ca8402445be
Kyle Tozer <kwtozer@gmail.com>
2018-11-04 11:02:12 -0500

    now tracking build directory

---
+++ build/precache-manifest.da01f27223f18ef8c25ea2d77a2d4300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "585c8d5676bae37b82be",
+    "url": "/static/js/main.585c8d56.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "/static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "585c8d5676bae37b82be",
+    "url": "/static/css/main.27e5ae53.chunk.css"
+  },
+  {
+    "revision": "440bf8098f4d43570d2bc399790e5d80",
+    "url": "/index.html"
+  }
+];
